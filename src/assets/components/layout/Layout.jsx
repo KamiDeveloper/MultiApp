bef661e4cf750141275c8c279c631c45f24a9d21
Kamiiuwu <medrano.9894276@gmail.com>
2025-08-27 20:46:45 -0400

    feat: Add multi-app layout with carousel and individual app components
    - Implemented a carousel to showcase multiple mini-applications including ClimaApp, GenCitas, and CalImc.
    - Created individual components for each app with corresponding styles.
    - Added a responsive layout with a header and social media links.
    - Integrated animated background with shooting stars and parallax effects.
    - Developed a button component for generating random quotes in GenCitas.
    - Styled components using CSS modules for better maintainability.
    - Configured Vite with React and Tailwind CSS for optimized development.

---
+++ src/assets/components/layout/Layout.jsx
@@ -0,0 +1,34 @@
+import Styles from './Layout.module.css';
+
+import { githubIcon, linkedinIcon, twitterIcon } from '../../icons/Icons';
+
+const Layout = ({ children }) => {
+    return (
+        
+        <div className={Styles.layout}>
+            <header className={Styles.headerContent}>
+                <div className={Styles.headerTitle}>
+                    KAMI
+                </div>
+                <div className={Styles.headerSocial}>
+                    <a href="https://github.com/KamiDeveloper" target="_blank" rel="noopener noreferrer" className={Styles.headerSocialLink}>
+                        {githubIcon()}
+                    </a>
+                    <a href="https://www.linkedin.com/in/jorge-medrano-ramirez/" target="_blank" rel="noopener noreferrer" className={Styles.headerSocialLink + " " + Styles.linkedin}>
+                        {linkedinIcon()}
+                    </a>
+                    <a href="https://x.com/KamiDev_" target="_blank" rel="noopener noreferrer" className={Styles.headerSocialLink}>
+                        {twitterIcon()}
+                    </a>
+                </div>
+            </header>
+            <div className={Styles.mainAppTitle}>
+                <h1 className={Styles.title}>Multi App</h1>
+                <p className={Styles.subtitle}>Colección de mini aplicaciones</p>
+            </div>
+            {children}
+        </div>
+    );
+};
+
+export default Layout;
